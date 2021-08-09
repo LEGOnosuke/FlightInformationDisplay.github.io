e3a45a92c85e2ebfc8aae9fadbcf6f5c3d2d7d53
LEGOnosuke <ohta.kura@gmail.com>
2021-08-09 23:21:48 +0900

    None

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-
-window.addEventListener('DOMContentLoaded' , function() {
-
-    let data;
-    let time = '12:50' ;
-    let where = 'SAPPORO';
-
-    data = where+time;
-
-    let html = '<tr><th>' + time + '</th><th>' + where + '</th></hr>';
-
-    document.querySelector('#output').innerHTML = html ;
-
-});
+document.getElementById('mydiv').innerHTML = "<p>こんばんわ！</p>";
